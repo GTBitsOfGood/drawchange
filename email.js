9b57ff52a076db9d90aaa83a208fdd4b5fd33617
kevintian <kevintian@gatech.edu>
2017-10-24 19:56:58 -0400

    Refactored email functionality into module

--- email.js
+++ email.js
@@ -1,39 +1,9 @@
-'use strict';
-const nodemailer = require('nodemailer');
+const Email = require('./backend/services/emailService.js');
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass  // generated ethereal password
-        }
-    });
+// from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+// to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+// subject: 'Hello ✔', // Subject line
+// text: 'Hello world?', // plain text body
+// html: '<b>Hello world?</b>' // html body
 
-    // setup email data with unicode symbols
-    const mailOptions = {
-        from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-        to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-});
+Email.sendEmail('"Fred Foo 👻" <foo@blurdybloop.com>', 'bar@blurdybloop.com, baz@blurdybloop.com', 'Hello ✔', '<bHello world?', 'This is a test');
