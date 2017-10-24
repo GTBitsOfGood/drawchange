028d9f554362847ca37b200b258280e88df6d0c3
vishalvijay30 <sinjaybrothers@gmail.com>
2017-10-24 19:54:49 -0400

    update indentation

--- backend/models/email.js
+++ backend/models/email.js
@@ -2,35 +2,35 @@ const mongoose = require('mongoose');
 
 // define schema for user collection (user model)
 const emailSchema = mongoose.Schema({
-  from: {
-    type: String,
-    required: true
-  },
-  subject: {
-    type: String,
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  isHTML: {
-    type: Boolean,
-    required: true
-  },
-  recipients: {
-    type: Array[ mongoose.Schema.Types.ObjectId ],
-    required: true
-  },
-  is_sent: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  deliver_on: {
-    type: Date,
-    required: true
-  }
+    from: {
+      type: String,
+      required: true
+    },
+    subject: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    recipients: {
+      type: Array[ mongoose.Schema.Types.ObjectId ],
+      required: true
+    },
+    is_sent: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    deliver_on: {
+      type: Date,
+      required: true
+    },
+    isHTML: {
+      type: Boolean,
+      required: true,
+    },
 }, { timestamps: true });
 
 // export Event model to app
