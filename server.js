94975b652f3306df5bf165c2204cadd559f47bae
kevintian <kevintian@gatech.edu>
2017-10-29 14:27:25 -0400

    Fixed previous bad merge

--- server.js
+++ server.js
@@ -9,15 +9,10 @@ require('dotenv').config(); // load env vars
 const PORT = process.env.PORT || 3000;
 const api = require('./backend/routes');
 
-<<<<<<< HEAD
-var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
-=======
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 mongoose.Promise = global.Promise;
->>>>>>> models
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,16 +31,5 @@ app.listen(PORT, error => {
     : console.info(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
 });
 
-<<<<<<< HEAD
-// Connection URL
-var url = 'dummy url';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to MongoDB!!");
-    db.close();
-});
-=======
 
 module.exports = app;
->>>>>>> models
