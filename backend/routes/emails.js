db097e6f2422ac68b1c555ab959fe1a2ff028de5
vishalvijay30 <sinjaybrothers@gmail.com>
2017-11-01 10:51:20 -0400

    finished testing email model

--- backend/routes/emails.js
+++ backend/routes/emails.js
@@ -1,5 +1,7 @@
 // NPM Packages
 const express = require('express');
+const { check, oneOf, validationResult } = require('express-validator/check');
+const { matchedData } = require('express-validator/filter');
 const router = express.Router();
 
 // Local Imports
@@ -12,48 +14,89 @@ router.route('/')
       .then(emails => res.status(200).json({ emails }))
       .catch(({ errors }) => res.status(500).json({ errors }));
   })
-  .post((req, res) => {
+  .post([
+    check('from').exists().isAscii().trim().escape(),
+    check('subject').exists().isAscii().trim().escape(),
+    check('text').exists().isAscii().trim().escape(),
+    check('recipients').exists(),
+    check('is_html').exists(),
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
     const { from, subject, text, recipients, is_sent, sent_on, is_html } = req.body;
     const newEmail = new Email({ from, subject, text, recipients, is_sent, sent_on, is_html });
     newEmail.save()
       .then(email => res.status(200).json({ email }))
-      .catch((errors) => { res.status(500).json({ errors });});
+      .catch((errors) => {
+        if (recipients.length == 0) {
+          res.status(422).json({ errors });
+        } else {
+          res.status(500).json({ errors });
+        }
+
+    });
   });
 
 router.route('/:id')
-  .get((req, res) => {
+  .get([
+      check('id').isMongoId()
+    ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
     Email.findById(req.params.id)
       .then(response => {
         response
           ? res.status(200).json({ response })
-          : res.status(404).json({ Error: 'No response found with id: ${req.params.id}'});
+          : res.status(404).json({ errors: 'No response found with id: ${req.params.id}'});
       })
       .catch(errors => { res.status(500).json({ errors }); });
   })
-  .put((req, res) => {
+  .put([check('id').isMongoId()], oneOf([
+    check('from').exists().isAscii().trim().escape(),
+    check('subject').exists().isAscii().trim().escape(),
+    check('text').exists().isAscii().trim().escape(),
+    check('recipients').exists(),
+    check('is_html').exists(),
+    ]), (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
     const { from, subject, text, recipients, is_sent, sent_on, is_html } = req.body;
     Email.findById(req.params.id)
       .then(response => {
-        response.from = from || response.from;
-        response.subject = subject || response.subject;
-        response.text = text || response.text;
-        response.recipients = recipients || response.recipients;
-        response.is_sent = is_sent || response.is_sent;
-        response.sent_on = sent_on || response.sent_on;
-        response.is_html = is_html || response.is_html;
-        response.save();
-        res.status(200).json({ response });
+        if (!response) {
+          res.status(404).json({ errors: errors.mapped() });
+        } else {
+           response.from = from || response.from;
+          response.subject = subject || response.subject;
+          response.text = text || response.text;
+          response.recipients = recipients || response.recipients;
+          response.is_sent = is_sent || response.is_sent;
+          response.sent_on = sent_on || response.sent_on;
+          response.is_html = is_html || response.is_html;
+          response.save();
+          res.status(200).json({ response });
+        }
       })
       .catch(({ errors }) => res.status(500).json({ errors }));
   })
-  .delete((req, res) => {
+  .delete([check('id').isMongoId()], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.mapped() });
+    }
     Email.findByIdAndRemove(req.params.id)
       .then(removed => {
         removed
-              ? res.status(200).json({removed})
-              : res.status(404).json({Error: "No response found with id: ${req.params.id}"});
+          ? res.status(200).json({removed})
+          : res.status(404).json({errors: "No response found with id: ${req.params.id}"});
       })
-      .catch(errors => { res.status(500).json({errors});});
+      .catch(errors => { res.status(500).json({errors})});
   });
 
 module.exports = router;
--- test/email.spec.js
+++ test/email.spec.js
@@ -11,50 +11,57 @@ const User = require('../backend/models/user');
 
 chai.use(chaiHttp);
 
-describe('Email Model', () => {
+const BAD_REQUEST_NO_RECIPIENTS = {
+  from: 'George Burdell',
+  subject: 'GT Bits of Good Kickoff',
+  text: 'Our first meeting will be tomorrow!',
+  is_sent: true,
+  sent_on: new Date('10/29/2017'),
+  is_html: true
+};
+
+const BAD_REQUEST_EMPTY_RECIPIENTS = {
+  from: 'George Burdell',
+  subject: 'Intro Meeting',
+  text: 'Come by tomorrow!',
+  recipients: [],
+  is_sent: true,
+  sent_on: new Date('05/29/2014'),
+  is_html: true
+};
+
+const GOOD_REQUEST = {
+  from: 'George Burdell',
+  subject: 'Intro Meeting',
+  text: 'Come by tomorrow!',
+  recipients: [new User('Jim', 'Acosta', 'jim@me.com', '25 Cox St', 'Atlanta', 'GA', '30332', '555.555.5555', new Date('10/12/1940'))],
+  is_sent: true,
+  sent_on: new Date('05/29/2014'),
+  is_html: true
+};
+
+const UPDATE = {
+  from: 'George'
+};
+
+describe('Email Model Tests', () => {
   describe('Create Email', () => {
     it('fails w/o all required fields', (done) => {
-      const fields = {
-        from: 'George Burdell',
-        subject: 'GT Bits of Good Kickoff',
-        text: 'Our first meeting will be tomorrow!',
-        is_sent: true,
-        sent_on: new Date('10/29/2017'),
-        is_html: true
-      };
-      const testEmail = new Email(fields);
+      const testEmail = new Email(BAD_REQUEST_NO_RECIPIENTS);
       testEmail.save((err) => {
         should.exist(err);
         done();
       });
     });
     it('fails with no recipients', (done) => {
-      const fields = {
-        from: 'George Burdell',
-        subject: 'Intro Meeting',
-        text: 'Come by tomorrow!',
-        recipients: [],
-        is_sent: true,
-        sent_on: new Date('05/29/2014'),
-        is_html: true
-      };
-      const testEmail = new Email(fields);
+      const testEmail = new Email(BAD_REQUEST_EMPTY_RECIPIENTS);
       testEmail.save((err) => {
         should.exist(err);
         done();
       });
     });
     it('works with all required fields', (done) => {
-      const fields = {
-        from: 'George Burdell',
-        subject: 'Intro Meeting',
-        text: 'Come by tomorrow!',
-        recipients: [new User('Jim', 'Acosta', 'jim@me.com', '25 Cox St', 'Atlanta', 'GA', '30332', '555.555.5555', new Date('10/12/1940'))],
-        is_sent: true,
-        sent_on: new Date('05/29/2014'),
-        is_html: true
-      };
-      const testEmail = new Email(fields);
+      const testEmail = new Email(GOOD_REQUEST);
       testEmail.save((err) => {
         should.not.exist(err);
         done();
@@ -67,12 +74,129 @@ describe('Email Model', () => {
         subject: 'GT Bits of Good Mandatory Email',
         is_html: false
       };
-      Email.findOneAndUpdate({from: 'George'}, fields, done);
+      Email.findOneAndUpdate(UPDATE, fields, done);
     });
   });
   describe('Deleting Email', () => {
     it('Email deleted', (done) => {
-      Email.findOneAndRemove({first_name: 'George'}, done);
+      Email.findOneAndRemove({from: 'George'}, done);
     });
   });
 });
+
+//REST endpoints test suite
+describe('Email REST Endpoints Test Suite', () => {
+
+  describe('GET /api/emails/', () => {
+    it('works with no body sent', (done) => {
+      chai.request(server)
+        .get('/api/emails/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('emails');
+          res.body.emails.should.be.a('array');
+          done();
+        });
+    });
+  });
+
+  describe('GET /api/emails/:id', () => {
+
+    it ('fails with a valid id that is not in DB', (done) => {
+      chai.request(server)
+        .get(`/api/emails/59f6130e6f22a25c10d72ce9`)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.have.property('errors');
+          done();
+        });
+    });
+
+    it ('fails with an invalid id', (done) => {
+      chai.request(server)
+        .get(`/api/emails/asdfaksdlj213lkj`)
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have.property('errors');
+          done();
+        });
+    });
+
+  });
+
+  describe('POST /api/emails/', () => {
+
+    it('works with proper body', (done) => {
+      chai.request(server)
+        .post('/api/emails/')
+        .send(GOOD_REQUEST)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('email')
+          res.body.email.should.be.a('object');
+          done();
+        });
+    });
+
+    it('fails with incorrect body', (done) => {
+      chai.request(server)
+        .post('/api/emails/')
+        .send(BAD_REQUEST_EMPTY_RECIPIENTS)
+        .end((err, res) => {
+          res.should.have.status(422);
+          done();
+        });
+    });
+  });
+
+  describe('PUT /api/emails/:id', () => {
+    it ('fails with an valid id that is not in DB', (done) => {
+      chai.request(server)
+        .put('/api/emails/59f6130e6f22a25c35d72ce9')
+        .send(UPDATE)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.have.property('errors');
+          done();
+        });
+    });
+    it ('fails with an invalid id ', (done) => {
+      chai.request(server)
+        .put('/api/emails/asdfaksdlj213lkj')
+        .send(UPDATE)
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have.property('errors');
+          done();
+        });
+    });
+  });
+
+  describe('DELETE /api/email/:id', () => {
+
+    it ('fails with a valid id that is not in DB', (done) => {
+      chai.request(server)
+        .delete(`/api/emails/59f6130e6f22a25c35d72ce9`)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.have.property('errors');
+          done();
+        });
+    });
+
+    it ('fails with an invalid id ', (done) => {
+      chai.request(server)
+        .delete(`/api/emails/asdfaksdlj213lkj`)
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have.property('errors');
+          done();
+        });
+    });
+
+  });
+
+});
+
+
+
