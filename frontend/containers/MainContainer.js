3abf005a543a8e79b90b5f2cf988a4fd5a901b68
Jagan Srini <jagsr8@gmail.com>
2017-11-11 14:21:06 -0500

    MainContainer boilerplate

--- frontend/containers/MainContainer.js
+++ frontend/containers/MainContainer.js
@@ -1,38 +1,75 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as eventActions from '../actions/events.js'
+import * as volunteerActions from '../actions/volunteers.js'
 import LeftPane from '../components/LeftPane';
 import MainPane from '../components/MainPane';
 import ItemList from '../components/ItemList';
 import EventView from '../components/EventView';
 
-const MainContainer = ({ name }) => {
-  return (
-        <LeftPane>
-            <ItemList />
-        </LeftPane>
-        <MainPane>
-          <EventView />
-        </MainPane>
-  );
+class MainContainer extends React.Component {
+  
+
+  render() {
+    let itemLists;
+    let mainItem;
+
+    switch( this.props.currentView ) {
+      case 'events':
+        itemLists = (
+          <ItemList title="Your Events" items={[]} updateCurrentEvent={} />
+          <ItemList title="Upcoming Events" items={this.props.events} updateCurrentEvent={} />
+        );
+        mainItem = (
+          <EventView mode={} event={} onCreate={} onUpdate={} onRegister={} onUnregister={} />
+        );
+        break;
+      case 'volunteers':
+        itemLists = (
+          <ItemList title="Volunteers" items={this.props.volunteers} updateCurrentEvent={} />
+        );
+        mainItem = (
+          {/*<VolunteerView />*/}
+        );
+        break;
+      default:
+        itemLists = ();
+        mainItem = ();
+        break;
+    }
+
+    return (
+      <LeftPane>
+        { itemLists }
+      </LeftPane>
+      <MainPane>
+        { mainItem }
+      </MainPane>
+    );
+  }
 };
 
-MainContainer.propTypes = {
-  name: PropTypes.string,
-};
-
-const mapStateToProps = (state) => {
+const mapStateToProps = ( state, ownProps ) => {
   return {
-    name: state.name
+    currentView: state.currentView,
+    events: state.events, // event data
+    volunteers: state.volunteers, // volunteer data
+    eventMode: state.eventMode, // view, edit, or create
+    currentEvent: state.currentEvent, // Event id
+    currentVolunteer: state.currentVolunteer, // Volunteer id
   };
 };
 
-const mapDispatchToProps = (/* dispatch */) => {
+const mapDispatchToProps = ( dispatch ) => {
   return {
+    eventActions: bindActionCreators( eventActions, dispatch ),
+    volunteerActions: bindActionCreators( volunteerActions, dispatch ),
   };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(MainContainer);
