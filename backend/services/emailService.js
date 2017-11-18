1d919bb2c81f88deca339f9104f98537e0666709
Kevin Tian <ytian78@gatech.edu>
2017-11-18 17:22:10 -0500

    Changed service to use sendgrid info

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,5 +1,6 @@
 'use strict';
 const nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
 
 // Local Imports & Constants
 require('dotenv').config(); // load env vars
@@ -9,21 +10,27 @@ function sendEmail(email) {
     // Generate test SMTP service account from ethereal.email
     // Only needed if you don't have a real mail account for testing
     nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-      const transporter = nodemailer.createTransport({
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        // host: process.env.SMTP_HOST,
-        // port: process.env.SMTP_PORT,
-        // secure: false, // true for 465, false for other ports
+      // Sendgrid info for SMTP
+      const options = {
         auth: {
-          user: account.user, // generated ethereal user
-          pass: account.pass  // generated ethereal password
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
         }
-      });
+      };
 
-    // setup email data with unicode symbols
+      // Settings for testing with local smtp
+      // const transporter = nodemailer.createTransport({
+      //   // host: account.smtp.host,
+      //   // port: account.smtp.port,
+      //   // secure: account.smtp.secure,
+      //   // host: process.env.SMTP_HOST,
+      //   // port: process.env.SMTP_PORT,
+      //   // secure: false, // true for 465, false for other ports
+      // });
+
+      const transporter = nodemailer.createTransport(sgTransport(options));
+
+      // setup email data with unicode symbols
       const mailOptions = {
         from: email.from, // sender address
         to: parseRecipientEmails(email.recipients), // list of receivers
@@ -32,7 +39,7 @@ function sendEmail(email) {
         html: email.text // html body
       };
 
-    // send mail with defined transport object
+      // send mail with defined transport object
       transporter.sendMail(mailOptions, (error) => {
         if (error) {
           return reject({
