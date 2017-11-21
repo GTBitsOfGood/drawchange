d4222dd6030c2f6cbf12c7ee8d673bf3a0bea4b3
Oberon <ahijaouy@gatech.edu>
2017-11-20 19:38:29 -0500

    initial commit

--- backend/models/question.js
+++ backend/models/question.js
@@ -2,13 +2,13 @@
  * Question Object is used to store survey questions
  */
 class Question {
-    constructor(text, type, choices) {
+  constructor(text, type, choices) {
     // Instance Fields for Question Object
-        this.type = type;
-        this.text = text;
-        this.choices = choices || null;
-        this.answer = null;
-    }
+    this.type = type;
+    this.text = text;
+    this.choices = choices || null;
+    this.answer = null;
+  }
 
 }
 
