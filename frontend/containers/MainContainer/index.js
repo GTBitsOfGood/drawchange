9d8685553367aca217598a70231923060269b10f
SujeethJinesh <sujeethjinesh@gmail.com>
2018-10-23 19:34:31 -0400

    [127] Fix Denied State

--- frontend/containers/MainContainer/index.js
+++ frontend/containers/MainContainer/index.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { AdminContainer } from "./admin";
 import { VolunteerContainer } from './volunteer';
 import { PendingContainer } from "./pending";
+import { DeniedContainer } from './denied';
 
 export const MainContainer = ({ user, ...props }) => {
   if (user && user.bio.role === "admin") {
@@ -12,6 +13,8 @@ export const MainContainer = ({ user, ...props }) => {
     return <VolunteerContainer {...props} />;
   } else if (user && user.bio.role === "pending") {
     return <PendingContainer {...props} />;
+  } else if (user && user.bio.role === "denied") {
+    return <DeniedContainer {...props} />;
   }
   return <h1>Something broke in Main Container index.js</h1>;
 };
