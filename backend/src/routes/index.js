e29f38d3cdc82dfb05cc66dedc026aa1ea9f0c14
Andre Hijaouy <hijaouya@gmail.com>
2018-12-19 20:48:09 -0500

    format backend code

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,14 +1,14 @@
-var express = require("express");
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get("/ping", function(req, res, next) {
-  res.send("We are live :) ");
+router.get('/ping', function(req, res, next) {
+  res.send('We are live :) ')
   // res.render("index", { title: "Express" });
-});
+})
 
-router.get("/test", (req, res) => {
-  res.send("testing 123");
-});
+router.get('/test', (req, res) => {
+  res.send('testing 123')
+})
 
-module.exports = router;
+module.exports = router
