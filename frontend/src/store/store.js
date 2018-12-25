fb70c9d9f1d7f6550d9d0922e87dd35f928dc11e
James Wang <jameswang@Jamess-MacBook-Pro.local>
2018-12-25 14:24:33 -0500

    adds redux and beginnings for navbar

---
+++ frontend/src/store/store.js
@@ -0,0 +1,13 @@
+/*
+ * src/store.js
+ * No initialState
+*/
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/RootReducer';
+export default function configureStore(initialState={}) {
+    return createStore(
+     rootReducer,
+      applyMiddleware(thunk)
+    );
+}
