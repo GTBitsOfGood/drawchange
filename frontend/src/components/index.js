2a0858fd5a66b707656d482454fdceb82b89c848
Andre Hijaouy <hijaouya@gmail.com>
2018-12-25 18:31:09 -0500

    add Pending Volunteer Screen

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,6 @@ import VolunteerApp from './VolunteerApp';
 import ProfileForm from './ProfileForm';
 import Header from './Header';
 import Splash from './Splash';
+import PendingVolunteer from './PendingVolunteer';
 
-export { VolunteerApp, ProfileForm, Header, Splash };
+export { VolunteerApp, ProfileForm, Header, Splash, PendingVolunteer };
