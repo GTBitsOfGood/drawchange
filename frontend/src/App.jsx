9a8410589a12c6219686c140145929a748416895
Andre Hijaouy <hijaouya@gmail.com>
2018-12-27 13:25:28 -0500

    start setting up App.jsx for demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { Header, VolunteerApp, AuthWall } from './components';
+import { Header, VolunteerApp, AuthWall, Splash } from './components';
 
 class App extends Component {
+  state = { authenticated: false };
+  componentWillMount(props) {
+    // check url for user id
+  }
+
+  fakeAuth = _ => this.setState({ authenticated: true });
+
   render() {
     const user = {};
-    const authenticated 
+
     return (
       <Router>
         <div>
           <Header />
+          {this.state.authenticated ? <h1>Hello</h1> : <Splash onAuth={this.fakeAuth} />}
 
-          <Route path="/" exact render={_ => <AuthWall user={user} />} />
+          {/* <Route path="/" exact render={_ => <AuthWall user={user} />} /> */}
           <Route path="/apply" exact render={_ => <VolunteerApp onSubmit={console.log} />} />
         </div>
       </Router>
